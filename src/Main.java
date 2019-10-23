1787f52d335cfd7db7740cf7d70d7d4a36900a13
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 15:44:52 +0800

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	int sum=0;
-	for(int i=1;i<100;i++){
-	    sum=sum+i;
-    }
-        System.out.println(sum);
+        Scanner scn=new Scanner(System.in);
+        float v1=scn.nextFloat();
+        float max=v1;
+        float min=v1;
+	for(int i=1;i<10;i++){
+	    v1=scn.nextFloat();
+	    if(max<v1){max=v1;
+        } else if (min > v1) {
+	        min=v1;
+        }
+        }
+        System.out.println("Max="+max+" "+"min="+min);
     }
 }
